3065cc9c992d837f7636f3c58c75d73cbaf1f62d
Tabytac <45774869+tabytac@users.noreply.github.com>
2024-05-04 20:01:01 +0100

    Add Demonym support for names

--- generate_newgrf.py
+++ generate_newgrf.py
@@ -1,5 +1,6 @@
 import math
 import os
+import csv
 import shutil
 import subprocess
 from datetime import datetime
@@ -34,9 +35,10 @@ BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 DATA_PATH = os.path.join(BASE_PATH, "Data")
 DATA_URL = "https://download.geonames.org/export/dump/"
 LIST_OF_DATA_FILES = ["countryInfo.txt", "admin1CodesASCII.txt", "admin2Codes.txt", "featureCodes_en.txt", "cities15000.zip", "cities5000.zip", "cities1000.zip", "cities500.zip"]
-GITHHUB_COUNTRY_DEMOONYM_URL = "https://raw.githubusercontent.com/mledoze/countries/blob/master/dist/countries.csv"
+GITHHUB_COUNTRY_DEMONYM_URL = "https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.csv"
+DEMONYM_COUNTRY_CODE_COLUMN = 3
+DEMONYM_COUNTRY_DEMONYM_COLUMN = 64
 NEWGRF_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.txt"
-#TODO: add the github url for the country demonyms file
 ID_FILE = os.path.join(BASE_PATH, "file_id.txt")
 
 
@@ -55,7 +57,7 @@ town_names {{
 """
 LANG_FILE_BOILERPLATE = """##grflangid 0x00
 STR_GRF_NAME                    :Taby [COUNTRY_NAME_A] Town Names {BLUE}v1.[VERSION]
-STR_GRF_DESC                    :{ORANGE}Taby [COUNTRY_NAME_A] Town Names{BLUE}v1.[VERSION]{}{}{WHITE}This NewGRF adds names for towns and cities[LOWEST_POPULATION] in [COUNTRY_NAME_B]. This set has over [NUM_TOWNS] towns with their spawn chance roughly based on the population of the town.{}{}The database of town names and other place names was taken from {LTBLUE}www.geonames.org.{}{}{BLACK}Made by: {GREEN}Tabytac{}{BLACK}Updated: {GREEN}[DATE]{}{}{SILVER}This grf is released under GNU GPL v3 or higher.
+STR_GRF_DESC                    :{ORANGE}Taby [COUNTRY_NAME_A] Town Names {BLUE}v1.[VERSION]{}{}{WHITE}This NewGRF adds names for towns and cities[LOWEST_POPULATION] in [COUNTRY_NAME_B]. This set has over [NUM_TOWNS] towns with their spawn chance roughly based on the population of the town.{}{}The database of town names and other place names was taken from {LTBLUE}www.geonames.org.{}{}{BLACK}Made by: {GREEN}Tabytac{}{BLACK}Updated: {GREEN}[DATE]{}{}{SILVER}This grf is released under GNU GPL v3 or higher.
 STR_GRF_URL                     :https://github.com/Tabytac/Taby-Town-Names
 STR_GAME_OPTIONS_TOWN_NAME      :Taby [COUNTRY_NAME_A] Town Names
 """
@@ -87,6 +89,15 @@ def get_country_region_subregion_names(country_code, region_code, subregion_code
         subregion_name = get_name(f"{country_code}.{region_code}.{subregion_code}", os.path.join(DATA_PATH, "admin2Codes.txt"), 1)
     return country_name, region_name, subregion_name
 
+def get_country_demonym(country_code):
+    if not os.path.exists(os.path.join(DATA_PATH, "countries.csv")):
+        download_data_files("countries.csv", GITHHUB_COUNTRY_DEMONYM_URL)
+    with open(os.path.join(DATA_PATH, "countries.csv"), 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for columns in reader:
+            if country_code == columns[DEMONYM_COUNTRY_CODE_COLUMN].strip('"').strip("'"):
+                return columns[DEMONYM_COUNTRY_DEMONYM_COLUMN].strip('"').strip("'")
+
 def prepare_output_dir(output_dir, country_code, region_code, subregion_code):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
@@ -95,8 +106,12 @@ def prepare_output_dir(output_dir, country_code, region_code, subregion_code):
     if not os.path.exists(os.path.join(output_dir, 'lang', 'english.lng')):
         with open(os.path.join(output_dir, 'lang', 'english.lng'), 'w', encoding='utf-8') as file:
             file.write(LANG_FILE_BOILERPLATE)
+    if not os.path.exists(os.path.join(BASE_PATH, "Source_Files", "license.txt")):
+        print("Downloading license file")
+        subprocess.run(["curl", "-o", os.path.join(BASE_PATH, "Source_Files", "license.txt"), NEWGRF_LICENSE_URL], check=True)
     if not os.path.exists(os.path.join(output_dir, 'license.txt')):
-        subprocess.run(["curl", "-o", os.path.join(output_dir, 'license.txt'), NEWGRF_LICENSE_URL], check=True)
+        print("Copying license file")
+        subprocess.run(["copy", os.path.join(BASE_PATH, "Source_Files", "license.txt"), os.path.join(output_dir, 'license.txt')], shell=True, check=True)
 
 
 def update_language_file(output_dir, country_code, region_code, subregion_code, version, num_towns, lowest_population):
@@ -112,29 +127,25 @@ def update_language_file(output_dir, country_code, region_code, subregion_code, 
     with open(lang_file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     country_name, region_name, subregion_name = get_country_region_subregion_names(country_code, region_code, subregion_code)
-    country_name_a = f"{country_name}".strip()
+    country_demonym = get_country_demonym(country_code)
+    country_demonym_a = f"{country_demonym}".strip()
     if region_name != "":
-        country_name_a = f"{country_name} {region_name}".strip()
-
+        country_demonym_a = f"{country_demonym_a} {region_name}".strip()
     if subregion_name != "":
-        country_name_a = f"{country_name} {region_name} {subregion_name}".strip()
-
-
-    # if the coutnry name is empty and the region code is too, then we are processing the world file
-    country_name_b = country_name_a
-    if country_name_a == "":
-        country_name_a = "World"
-        country_name_b = "the world"
+        country_demonym_a = f"{country_demonym_a} {region_name} {subregion_name}".strip()
+    country_demonym_b = f"{country_name} {region_name} {subregion_name}".strip()
+    if country_demonym_a == "None":
+        country_demonym_a = "World"
+        country_demonym_b = "the World"
 
     current_date = datetime.now().strftime("%Y-%m-%d")
 
-    content = content.replace("[COUNTRY_NAME_A]", country_name_a)
-    content = content.replace("[COUNTRY_NAME_B]", country_name_b)
+    content = content.replace("[COUNTRY_NAME_A]", country_demonym_a)
+    content = content.replace("[COUNTRY_NAME_B]", country_demonym_b)
     content = content.replace("[VERSION]", str(version))
     content = content.replace("[DATE]", current_date)
     content = content.replace("[NUM_TOWNS]", str(num_towns))
     content = content.replace("[LOWEST_POPULATION]", lowest_population)
-
     with open(lang_file_path, 'w', encoding='utf-8') as file:
         file.write(content)
 
@@ -182,7 +193,7 @@ def determine_input_data(country_code):
     # Check if merged file should be used
     if MERGED_FILE_OVERIDE or country_code == "":
         if not os.path.exists(os.path.join(DATA_PATH, "allCountries.txt")):
-            download_data_files("allCountries.zip")
+            download_data_files("allCountries.zip", DATA_URL + "allCountries.zip")
         file_path = os.path.join(DATA_PATH, "allCountries.txt")
     else:
         file_path = os.path.join(DATA_PATH, f"Data/{country_code}.txt")
@@ -192,7 +203,6 @@ def determine_input_data(country_code):
         os.makedirs(os.path.join(DATA_PATH, "Data"), exist_ok=True)
     if not os.path.exists(file_path):
         download_and_extract_country_file(country_code, file_path)
-    
     return file_path
 
 def download_and_extract_country_file(country_code, file_path):
@@ -217,9 +227,8 @@ def download_and_extract_country_file(country_code, file_path):
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"Expected data file not found: {file_path}")
 
-def download_data_files(file_name):
+def download_data_files(file_name, download_url):
     file_path = os.path.join(DATA_PATH, file_name)
-    download_url = DATA_URL + file_name
     print(f"Downloading {download_url}")
     subprocess.run(["curl", "-o", file_path, download_url], check=True)
     if file_name.endswith(".zip"):
@@ -371,13 +380,14 @@ def split_input(code):
     return country_code, region_code, subregion_code
 
 def main():
-
     if not os.path.exists(DATA_PATH):
         os.makedirs(DATA_PATH, exist_ok=True)
+    if not os.path.exists(os.path.join(BASE_PATH, "Source_Files")):
+        os.makedirs(os.path.join(BASE_PATH, "Source_Files"), exist_ok=True)
     for file in LIST_OF_DATA_FILES:
         file_text = file.replace(".zip", ".txt")
         if not os.path.exists(os.path.join(DATA_PATH, file_text)):
-            download_data_files(file)
+            download_data_files(file, DATA_URL + file)
             print(f"Downloaded {file}")
     if input("Do you want to use the data input list? (yes = y, no = n): ") == "y":
         data_input = get_input(DATA_INPUT)
@@ -385,9 +395,13 @@ def main():
         data_input = take_input()
     print("Data input list:")
     print(data_input)
+    i = 0
     for code in data_input:
         country_code, region_code, subregion_code = split_input(code)
         process_country_region(country_code, region_code, subregion_code)
+        i += 1
+        print (f"Processed {i} of {len(data_input)}")
 
 if __name__ == "__main__":
     main()
+
